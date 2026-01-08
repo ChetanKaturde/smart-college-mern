e628e4185c27dad41056dd6dd91f031e9fb39fd8
Rutika Khabale <rutikakhabale8@gmail.com>
2026-01-08 18:28:46 +0530

    Complete backend implementation with created modules and check APIs also

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -1,13 +1,4 @@
-// const Department = require("../models/Department");
 
-// exports.create = async (req, res) => {
-//   const dept = await Department.create(req.body);
-//   res.status(201).json(dept);
-// };
-
-// exports.list = async (req, res) => {
-//   res.json(await Department.find());
-// };
 
 
 
